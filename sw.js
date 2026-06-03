d15e5421f56373fa4a7c9402d856b52e5c1746d7
alexandermangawhai-wq <alexander.mangawhai@gmail.com>
2026-06-03 20:11:26 +1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,2 @@
-const CACHE = 'ironlog-v2';
-const ASSETS = ['/ironlog/', '/ironlog/index.html', '/ironlog/style.css', '/ironlog/app.js', '/ironlog/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
